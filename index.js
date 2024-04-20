f44e62060806ea71989ba0c07b208783ed98296b
Ayushkt02 <ayushkumartiwary_it22a8_73@dtu.ac.in>
2024-04-20 11:48:02 +0530

    Upload

---
+++ index.js
@@ -0,0 +1,167 @@
+const words = 'A grade student Ankur who embarked on a journey of mentorship with me This students eagerness to learn and grow was evident from the very beginning Together we navigated the challenges of academics developing effective study strategies and refining their understanding of various subjects Beyond the classroom this student exhibited remarkable personal growth gaining confidence in their abilities and discovering their unique strengths It has been a privilege to witness his transformation and provide guidance along the way With his unwavering determination and commitment to excellence I have no doubt that this student is destined for great achievements I am immensely proud of his progress and honored to have played a part in their educational journey'.split(' ');
+const wordCount =  words.length;
+
+const gameTime = 30 * 1000;
+window.timer = null;
+window.gameStart = null;
+
+
+
+function randomWord(){
+  const randomIndex = Math.ceil(Math.random() * wordCount);
+  return words[randomIndex - 1];
+}
+
+function addClass(el, name){
+  el.className += ' '+name;
+}
+
+function removeClass(el, name){
+  el.className = el.className.replace(name, '');
+}
+
+function formatWord(word){
+  return `<div class="word"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
+}
+
+function newGame(){
+  document.getElementById("words").innerHTML = '';
+  for (let i = 0; i < 200; i++){
+    document.getElementById('words').innerHTML +=formatWord(randomWord());
+  }
+  addClass(document.querySelector('.word'), 'current');
+  addClass(document.querySelector('.letter'), 'current');
+  document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+  window.timer = null;
+}
+
+function getWpm() {
+  const words = [...document.querySelectorAll('.word')];
+  const lastTypedWord = document.querySelector('.word.current');
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
+  const typedWords = words.slice(0, lastTypedWordIndex);
+  const correctWords = typedWords.filter(word => {
+    const letters = [...word.children];
+    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+    const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+    return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+  });
+  return correctWords.length / gameTime * 60000;
+}
+
+function gameOver(){
+  clearInterval(window.timer);
+  addClass(document.getElementById('game'), 'over');
+  const result = getWpm();
+  document.getElementById('info').innerHTML = `WPM : ${result}`;
+}
+
+document.getElementById('game').addEventListener('keydown', function(ev){
+  const key = ev.key;
+  const currentWord = document.querySelector('.word.current')
+  const currentLetter = document.querySelector('.letter.current');
+  const expected = currentLetter?.innerHTML || ' ';
+  const isLetter = key.length === 1 && key !== ' ';
+  const isSpace = key === ' ';
+  const isBackspace = key === 'Backspace';
+  const isFirstLetter = currentLetter === currentWord.firstChild;
+  const isExtra = document.querySelector('.letter.incorrect.extra');
+
+  if(document.querySelector('#game.over')){
+    return;
+  }
+
+//countdown
+  if(!window.timer && isLetter){
+    window.timer = setInterval(() => {
+      if(!window.gameStart){
+        window.gameStart = (new Date()).getTime();
+      }
+      const currentTime = (new Date()).getTime();
+      const msPassed = currentTime - window.gameStart;
+      const sPassed = Math.round(msPassed / 1000);
+      const sLeft = (gameTime/1000) - sPassed;
+      if(sLeft <= 0){
+        gameOver();
+        return;
+      }
+      document.getElementById('info').innerHTML = sLeft + '';
+    }, 1000)
+  }
+
+  if(isLetter){
+    if(currentLetter){
+      addClass(currentLetter, key === expected ? 'correct':'incorrect');
+      removeClass(currentLetter, 'current');
+      if(currentLetter.nextSibling){
+        addClass(currentLetter.nextSibling, 'current');
+      }
+    }
+    else{
+        const incorrectLetter = document.createElement('span');
+        incorrectLetter.innerHTML = key;
+        incorrectLetter.className = 'letter incorrect extra';
+        currentWord.appendChild(incorrectLetter);
+    }
+  }
+  if(isSpace){
+    if(expected !== ' '){
+      const lettersToInvalidate =[...document.querySelectorAll('.word.current .letter:not(.correct)')];
+      lettersToInvalidate.forEach(letter => {
+        addClass(letter, 'incorrect');
+      });
+    }
+    removeClass(currentWord, 'current');
+    addClass(currentWord.nextSibling, 'current');
+    if(currentLetter){
+      removeClass(currentLetter, 'current');
+    }
+    addClass(currentWord.nextSibling.firstChild, 'current');
+  }
+
+  if(isBackspace){
+    if(currentLetter && isFirstLetter){
+      // move to previous word
+      removeClass(currentWord, 'current');
+      addClass(currentWord.previousSibling, 'current');
+      //move current letter to last letter of the previous word
+      removeClass(currentLetter, 'current');
+      addClass(currentWord.previousSibling.lastChild, 'current');
+      removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+      removeClass(currentWord.previousSibling.lastChild, 'correct');
+    }
+    if(currentLetter && !isFirstLetter){
+      removeClass(currentLetter, 'current');
+      addClass(currentLetter.previousSibling, 'current');
+      removeClass(currentLetter.previousSibling, 'correct');
+      removeClass(currentLetter.previousSibling, 'incorrect');
+    }
+    if(!currentLetter){
+      addClass(currentWord.lastChild, 'current');
+      removeClass(currentWord.lastChild, 'incorrect');
+      removeClass(currentWord.lastChild, 'correct');
+    }
+    if(isExtra){
+      currentWord.removeChild(isExtra);
+    }
+  }
+
+  // scrolling 
+  if(currentWord.getBoundingClientRect().top > 250){
+    const words = document.getElementById('words');
+    const margin = parseInt(words.style.marginTop || '0px');
+    words.style.marginTop = (margin - 70) + 'px';
+  }
+
+
+  //move cursor
+  const nextLetter = document.querySelector('.letter.current');
+  const nextWord = document.querySelector('.word.current')
+  const cursor = document.getElementById('cursor');
+  cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 +'px';
+  cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left':'right'] +'px';
+
+});
+
+
+newGame();
